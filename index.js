70f89a2d69150f8e8178863fa133c6e54dfa4df8
JohnS <senakj2011@hotmail.com>
2020-03-25 19:33:42 -0400

    added question

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+inquirer.prompt([
+    {
+       type: "input",
+       name: "title",
+       message: "What is the title of this project?" 
+    },
+    {
+        type: "input",
+
+    }
+    
+]).then(function(response) {
+    console.log(response.title);
+    fs.appendFile("README.md", response.title, function(err){
+        if(err){
+            return console.log(err);
+        }
+        console.log("Success!");
+    })
+});
+
+
 const questions = [
 
 ];
