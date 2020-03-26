cddf9a6d7207f1f3b8a92c2043867f249c7f162e
JohnS <senakj2011@hotmail.com>
2020-03-25 21:44:45 -0400

    finished questions

--- index.js
+++ index.js
@@ -4,18 +4,49 @@ var fs = require("fs");
 inquirer.prompt([
     {
        type: "input",
-       user: "userName",
+       name: "userName",
        message: "What is your Github username?" 
     },
     {
         type: "input",
         name: "projectName",
         message: "What is your project name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project:"
+    },
+    {
+        type: "list",
+        name: "projectLicense",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "installDependancies",
+        message: "What command should be run to install dependancies?"
+    },
+    {
+        type: "input",
+        name: "runTests",
+        message: "What command should be used to run tests?"
+    },
+    {
+        type: "input",
+        name: "repoInfo",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "repoContribute",
+        message: "What does the user need to know about contributing to the repo?"
     }
     
 ]).then(data => {
     console.log(data);
-    fs.appendFile("README.md", response, function(err){
+    fs.appendFile("README.md", data, err => {
         if(err){
             return console.log(err);
         }
